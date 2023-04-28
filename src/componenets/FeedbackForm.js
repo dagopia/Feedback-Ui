3eee3dce56262caaa1be874220fcc68943ca631e
dagopia <tdaguti@gmail.com>
2023-04-28 07:43:41 -0700

    feedback form and Rating select radio input added to feddback UI app.

--- src/componenets/FeedbackForm.js
+++ src/componenets/FeedbackForm.js
@@ -1,10 +1,26 @@
 import { Card, Typography, Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
-
+import RatingSelect from "./RatingSelect";
 const FeedbackForm = () => {
   const [text, SetTtext] = useState("");
+  const [rating, SetRating] = useState(10);
+
+  const [btnDisabled, SetbtnDisabled] = useState(true);
+
+  const [message, SetMessage] = useState("");
+
   const handleChange = (e) => {
+    if (text === "") {
+      SetbtnDisabled(true);
+      SetMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      SetMessage("Text must be at least 10 characters");
+      SetbtnDisabled(true);
+    } else {
+      SetMessage(null);
+      SetbtnDisabled(false);
+    }
     SetTtext(e.target.value);
   };
   return (
@@ -22,6 +38,7 @@ const FeedbackForm = () => {
     >
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => SetRating(rating)} />
         <Box>
           <TextField
             type='text'
@@ -33,11 +50,13 @@ const FeedbackForm = () => {
           <Button
             type='submit'
             variant='contained'
+            disabled={btnDisabled}
             sx={{ backgroundColor: "#ff6a95", color: "white" }}
           >
             Send
           </Button>
         </Box>
+        {message && <Box>{message}</Box>}
       </form>
     </Card>
   );
