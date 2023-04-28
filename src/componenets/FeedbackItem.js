4976f0508548e3a6016c51e6b3876ef2e16fe9fd
dagopia <tdaguti@gmail.com>
2023-04-28 04:59:33 -0700

    Feedback item componenet.

--- src/componenets/FeedbackItem.js
+++ src/componenets/FeedbackItem.js
@@ -1,14 +1,16 @@
 import { Button } from "@mui/base";
-import { Card, Typography } from "@mui/material";
+import { Card, Typography, Box } from "@mui/material";
 import React from "react";
 import { useState } from "react";
-const FeedbackItem = ({ item }) => {
+import CloseIcon from "@mui/icons-material/Close";
+const FeedbackItem = ({ item, handleDelete }) => {
   const handleClick = () => {
     setRating((prev) => {
       console.log(prev);
       return prev + 1;
     });
   };
+
   const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is Example of Feedback item");
 
@@ -18,11 +20,12 @@ const FeedbackItem = ({ item }) => {
       sx={{
         backgroundColor: "white",
         color: "#ff6a95",
-        width: "400px",
+        width: "300px",
         fontWeight: "bold",
         borderRadius: "18px",
         m: 2,
         p: 2,
+        display: "flex",
       }}
     >
       <Typography
@@ -34,15 +37,20 @@ const FeedbackItem = ({ item }) => {
           bgcolor: "#ff6a95",
           color: "white",
           p: 1,
+          border: "1px solid white",
           margin: "-25px",
         }}
       >
         {item.rating}
       </Typography>
-      <Typography className='text-display' sx={{ p: 2, mx: 3, color: "black" }}>
+      <Typography className='text-display' sx={{ mx: 3, color: "black" }}>
         {item.text}
       </Typography>
-      <Button onClick={handleClick}>Click</Button>
+      <Box
+        sx={{ display: "flex", justifyContent: "flex-end", alignItems: "left" }}
+      >
+        <CloseIcon tyope='button' onClick={(id) => handleDelete(item.id)} />
+      </Box>
     </Card>
   );
 };
