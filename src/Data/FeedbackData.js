cd209351a8203a2a9520715a5000c98ffb8f34e2
dagopia <tdaguti@gmail.com>
2023-04-28 05:00:26 -0700

    Feedback Array of Data.

--- src/Data/FeedbackData.js
+++ src/Data/FeedbackData.js
@@ -1,7 +1,24 @@
 const FeedbackData = [
-  { id: 1, rating: 8, text: "bla bla bla" },
-  { id: 2, rating: 9, text: "bla bla bla" },
-  { id: 3, rating: 10, text: "bla bla bla" },
-  { id: 4, rating: 11, text: "bla bla bla" },
+  {
+    id: 1,
+    rating: 8,
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printe.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
+  },
+  {
+    id: 3,
+    rating: 10,
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
+  },
+  {
+    id: 4,
+    rating: 11,
+    text: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printe.",
+  },
 ];
+
 export default FeedbackData;
