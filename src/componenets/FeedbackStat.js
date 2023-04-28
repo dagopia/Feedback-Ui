4679ad9309b33621a15ede28c837a44a2e28b5e3
dagopia <tdaguti@gmail.com>
2023-04-28 06:48:58 -0700

    Feedbabck UI review status componenet.

---
+++ src/componenets/FeedbackStat.js
@@ -0,0 +1,16 @@
+import { Box } from "@mui/system";
+import React from "react";
+
+const FeedbackStat = ({ feedback }) => {
+  let avarage = feedback.reduce((acc, cur) => {
+    return acc + cur.rating;
+  }, 0);
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Avarage Rating:{isNaN(avarage) ? 0 : avarage}</h4>
+    </Box>
+  );
+};
+
+export default FeedbackStat;
