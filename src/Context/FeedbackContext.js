8b7b614b655ffe45091197c1867290f0cb20068e
dagopia <tdaguti@gmail.com>
2023-05-04 07:11:56 +0300

    Update feed back item.

--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -8,21 +8,46 @@ export const FeedbackProvider = ({ children }) => {
     { id: 2, text: "This  is Feedback item2 ", rating: 9 },
     { id: 3, text: "This  is Feedback item3 ", rating: 8 },
   ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+  //add feedback item
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
+  //delete fedd back item
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure ypu wnat to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
+  //update feedback item
+  const updateFeedback = (id, updatedItem) => {
+    setFeedback(
+      feedback.map((item) =>
+        item.id === id ? { ...item, ...updatedItem } : item
+      )
+    );
+  };
+  //function to set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
